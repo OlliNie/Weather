66ca2ffe78110e0ae87caa92c8c2390d1d318bc9
ollinieminen <otn987@gmail.com>
2020-05-30 13:49:10 +0300

    nono

---
+++ WeatherMachine/configuration/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.firebaseConfig = void 0;
+exports.firebaseConfig = {
+    apiKey: "AIzaSyBu8mVbVZ2cCgjU1XI_5e00qsBgJRvWYYw",
+    authDomain: "weather-8654c.firebaseapp.com",
+    databaseURL: "https://weather-8654c.firebaseio.com",
+    projectId: "weather-8654c",
+    storageBucket: "weather-8654c.appspot.com",
+    messagingSenderId: "1069979341863",
+    appId: "1:1069979341863:web:8f0ff2dcdcbca1b63dea02",
+    measurementId: "G-EY9BZ09347",
+};
---
+++ WeatherMachine/configuration/firebaseConfig/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBu8mVbVZ2cCgjU1XI_5e00qsBgJRvWYYw",
+  authDomain: "weather-8654c.firebaseapp.com",
+  databaseURL: "https://weather-8654c.firebaseio.com",
+  projectId: "weather-8654c",
+  storageBucket: "weather-8654c.appspot.com",
+  messagingSenderId: "1069979341863",
+  appId: "1:1069979341863:web:8f0ff2dcdcbca1b63dea02",
+  measurementId: "G-EY9BZ09347",
+};
