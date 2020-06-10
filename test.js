1ff7d01ec3c37afd62cd036f1ca124bee2f619f9
ollinieminen <otn987@gmail.com>
2020-06-10 08:47:44 +0300

    text with waveshare

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = require("dotenv");
 var waveshare_1 = require("./WeatherMachine/helpers/waveshare/waveshare");
-var waveShare = new waveshare_1.Waveshare("/dev/ttyS0");
-var textParams = {
-    number: "0444036147",
-    message: "programmatically awesome!",
-};
-waveShare.text(textParams);
+dotenv_1.config();
+var phoneNumber = process.env.PHONE_NUMBER;
+if (phoneNumber) {
+    var waveShare = new waveshare_1.Waveshare("/dev/ttyS0");
+    var textParams = {
+        number: phoneNumber,
+        message: "programmatically awesome!",
+    };
+    waveShare.text(textParams);
+}
