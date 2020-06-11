91e360ddc86be538acf1b36964b5fe3b4894aedd
ollinieminen <otn987@gmail.com>
2020-06-11 19:40:16 +0300

    a

--- WeatherMachine/helpers/waveshare/waveshare.ts
+++ WeatherMachine/helpers/waveshare/waveshare.ts
@@ -42,7 +42,7 @@ export class Waveshare {
 
   powerOn = () => {
     gpio
-      .setup(7, gpio.DIR_LOW)
+      .setup(7, gpio.DIR_OUT, gpio.EDGE_NONE)
       .then(() => gpio.read(7))
       .then((res) => {
         console.log("initial state:", res);
