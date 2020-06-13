40e91dcb6262b36df5f69f5be1aaac5c7733ded1
ollinieminen <otn987@gmail.com>
2020-06-13 15:46:37 +0300

    a

--- WeatherMachine/helpers/waveshare/waveshare.ts
+++ WeatherMachine/helpers/waveshare/waveshare.ts
@@ -126,7 +126,7 @@ export class Waveshare {
           const connected = await this.port.write("AT+CGACT=1", 30000);
 
           console.log("connected", connected);
-          const check = await this.port.write("AT+CGDCONT=?", 15000);
+          const check = await this.port.write("AT+CGDCONT?", 15000);
           console.log("connection status", check);
         })
         .then(() => res(true));
